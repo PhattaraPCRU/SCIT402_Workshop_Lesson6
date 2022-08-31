93aef099e0559c611137f4867c2e820413ea74be
PhattaraPCRU Nitro5 <st641102064111@office365.pcru.ac.th>
2022-08-31 16:20:07 +0700

    .

---
+++ src/main/java/ccyExchange/Exchange.java
@@ -0,0 +1,22 @@
+
+package ccyExchange;
+import java.util.Scanner;
+
+/**
+ *
+ * @author koonp@641102064111
+ * Create on 11:43:14 AM Aug 31, 2022
+ * This file is part of "ccyExchange" Package.
+ */
+public class Exchange {
+    public void getdata(){
+        Scanner scn = new Scanner(System.in);
+        double ccy = -1;
+        while(ccy!=0){
+            ccy = scn.nextDouble();
+            
+        }
+    }
+
+}
+
---
+++ src/main/java/phattara/scit402_workshop_lesson6/SCIT402_Workshop_Lesson6.java
@@ -0,0 +1,18 @@
+/**
+ *
+ * @author koonp@641102064111
+ * Create on 10:51:07 AM Aug 31, 2022
+ * This file is part of "phattara.scit402_workshop_lesson6" Package (First).
+ */
+package phattara.scit402_workshop_lesson6;
+
+/**
+ *
+ * @author koonp
+ */
+public class SCIT402_Workshop_Lesson6 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
